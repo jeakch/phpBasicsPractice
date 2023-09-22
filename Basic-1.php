1f39d10aebc617deee3988c8e94ed73569aa2e6d
jeakch <mc297618@163.com>
2023-09-22 19:03:03 +0800

    公司提交第二天

--- Basic-1.php
+++ Basic-1.php
@@ -62,11 +62,41 @@ echo $z;
 ?>
 
 <?php
-    echo "---------php常量----------";
+    echo "---------php常量----------<br>";
     // 1. 使用函数定义一个常量 define
-    define('PI',3.14)
+    define('PI',3.14);
+   
 
-    // 
+    // 2. 使用const关键字定义
+    const PI2 = 3;
+
+    echo "--定义特殊常量， 用define; 不能直接用const, 可以用constant, 例如consttant(-_-)<br>";
+    define ('-_-','smile');
+
+
+    // 3. 系统常量
+    echo  "系统常量 PHP_VERSION =》PHP版本号：".PHP_VERSION;
+    echo  "<hr>";
+
+    // 4. 魔术常量
+    echo "----魔术常量--- ：由双下划线+常量名+双下划线组成，其值会随着环境变化而变化，用户无法改变--<br>";
+    echo "_DIR_:当前被执行的脚本所在电脑的绝对路径: ".__DIR__ ;
+    echo "<br>";
+    // echo "_FILE_:当前被执行的脚本所在的电脑的绝对路径(带自己文件的名字)"._FILE_;
+    echo "<br>";
+    echo "_LINE_:当前所属的行数: ".__LINE__ ;
+    echo "<br>";
+    echo "<br>";
+
+
+
+
+    // todo: PHP的八种数据类型
+    echo '--php数据类型8种--:<br>基本数据类型（4类），复杂数据类型（2类），特殊数据类型：2类（资源类型；空类型）'
+    //! 资源类型：resource , 存放资源数据（PHP外部数据，如数据库、文件）
+
+
+    
 ?>
 
 
@@ -80,7 +110,7 @@ echo $z;
 // foreach 仅能够应用于数组和对象，
     $array = [
     [1,2],
-    [3,4],
+    [3,4], 
  ]
 
 ?>
@@ -92,6 +122,10 @@ echo $z;
   }
   $b = $a = 5;
   $f = double($b++);
-  // echo "$b<br>";
+  echo "<hr>";
   echo "double($b++)的结果是：".$f;
 ?>
+
+
+
+
